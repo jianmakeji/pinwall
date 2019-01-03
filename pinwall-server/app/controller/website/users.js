b7fab3dcc7cf27db7ad06c8fb392f1ba87cc7d1e
codeliling <maximuslee@126.com>
2019-01-03 17:03:52 +0800

    fix bugs

--- pinwall-server/app/controller/website/users.js
+++ pinwall-server/app/controller/website/users.js
@@ -216,6 +216,7 @@ class UsersController extends BaseController{
     const email = ctx.request.body.email;
     const result = await ctx.service.users.bindWeixinInfoByEmail(email,ctx.user);
     if (result){
+      ctx.logout();
       super.success('绑定成功，请进入邮箱激活!');
     }
     else{
@@ -265,6 +266,7 @@ class UsersController extends BaseController{
         try{
           const result = await ctx.service.users.createUser(user,1);
           if (result){
+            ctx.logout();
             super.success('操作成功！请到邮箱激活');
           }
           else{
