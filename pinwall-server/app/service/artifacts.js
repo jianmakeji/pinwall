1ff10370290bfa7fda4ecc87d11aaff279f180ac
gansiqiu <1223720954@qq.com>
2019-01-07 10:28:37 +0800

    fix bug

--- pinwall-server/app/service/artifacts.js
+++ pinwall-server/app/service/artifacts.js
@@ -65,12 +65,16 @@ class Artifacts extends Service {
     let transaction;
     try {
       transaction = await this.ctx.model.transaction();
+      artifact.visible = 0;
       const artiObj = await this.ctx.model.Artifacts.createArtifact(artifact,transaction);
-      await this.ctx.model.TopicArtifact.createTopicArtifact(
-          {
-            artifactId:artiObj.Id,
-            topicId:artifact.topicId
-          },transaction);
+      if (artifact.topicId != 0){
+          await this.ctx.model.TopicArtifact.createTopicArtifact(
+              {
+                artifactId:artiObj.Id,
+                topicId:artifact.topicId
+              },transaction);
+      }
+
       let terms = artifact.terms;
       for (let term of terms){
         const termObj = await this.ctx.model.Terms.createTerm(term,transaction);
@@ -257,7 +261,28 @@ class Artifacts extends Service {
   }
 
   async getPersonalJobByUserId(query) {
-    return await this.ctx.model.Artifacts.getPersonalJobByUserId(query);
+    let resultObj = await this.ctx.model.Artifacts.getPersonalJobByUserId(query);
+    const app = this.ctx.app;
+    resultObj.rows.forEach((element, index)=>{
+      if (element.profileImage.indexOf('pinwall.fzcloud') == -1){
+        element.profileImage = app.signatureUrl(app.imagePath + element.profileImage, "thumb_360_360");
+
+        for (let subElement of element.artifact_assets){
+          subElement.profileImage = app.signatureUrl(app.imagePath + subElement.profileImage, "thumb_1000");
+          if (subElement.type == 2 && subElement.mediaFile != null){
+            subElement.mediaFile = app.signatureUrl(app.pdfPath + subElement.mediaFile);
+          }
+          else if (subElement.type == 3 && subElement.mediaFile != null){
+            subElement.mediaFile = app.signatureUrl(app.rar_zipPath + subElement.mediaFile);
+          }
+          else if (subElement.type == 4 && subElement.mediaFile != null){
+            subElement.mediaFile = app.signatureUrl(app.videoPath + subElement.mediaFile);
+          }
+        }
+      }
+
+    });
+    return resultObj;
   }
 
   async transferArtifacts() {
