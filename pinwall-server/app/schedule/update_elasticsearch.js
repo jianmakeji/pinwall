250bc25adbe3f0b9f3d9bb1022ce55e83999e793
gansiqiu <1223720954@qq.com>
2019-01-14 09:08:04 +0800

    Update update_elasticsearch.js

--- pinwall-server/app/schedule/update_elasticsearch.js
+++ pinwall-server/app/schedule/update_elasticsearch.js
@@ -11,7 +11,7 @@ class UpdateElasticsearch extends Subscription {
 
   // subscribe 是真正定时任务执行时被运行的函数
   async subscribe() {
-    concole.log('hello,world!');
+    console.log('hello,world!');
   }
 }
 
