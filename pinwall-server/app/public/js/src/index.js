9cdad4acd467ebaae63f7fa6d9df1edec05a2dc5
gansiqiu <1223720954@qq.com>
2019-01-21 09:12:43 +0800

    增加浏览器判断

--- pinwall-server/app/public/js/src/index.js
+++ pinwall-server/app/public/js/src/index.js
@@ -1,6 +1,6 @@
 var index = new Vue({
     el: '.index',
-    data(){
+    data:function(){
         return{
             containerStyle:{
                 width: "100%",
@@ -13,14 +13,11 @@ var index = new Vue({
             dataList:[]
         }
     },
-    methods: {
-
-    },
-    created(){
+    created:function(){
         this.$Loading.start();
         this.containerStyle.marginTop = (document.documentElement.clientHeight - 100 - 500 - 50 ) / 2 + "px";
-
         var that = this;
+        isChorme(this);
         this.$http({
             url: config.ajaxUrls.getIndexData,
             method:"GET",
@@ -31,7 +28,7 @@ var index = new Vue({
             if (res.status == 200) {
                 that.$Loading.finish();
                 that.dataList = res.body;
-                for(let i=0; i < that.dataList.length; i++){
+                for(var i=0; i < that.dataList.length; i++){
                     that.dataList[i].createAt = that.dataList[i].createAt.split("T")[0];
                 }
             }
@@ -40,3 +37,9 @@ var index = new Vue({
         })
     }
 })
+function isChorme(that){
+    console.log(navigator.userAgent.toLowerCase());
+	if(navigator.userAgent.toLowerCase().indexOf("chrome") == -1 && navigator.userAgent.toLowerCase().indexOf("firefox") == -1){
+		that.$Message.error({content:"为了有更好的使用体验，推荐使用谷歌或者火狐浏览器！",closable:true,duration:0});
+	}
+}
