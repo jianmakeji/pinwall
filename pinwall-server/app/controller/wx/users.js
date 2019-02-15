bf7ac0dc1e4645cf759fef778b31241326ea4470
codeliling <maximuslee@126.com>
2019-02-15 10:14:20 +0800

    fix bugs

--- pinwall-server/app/controller/wx/users.js
+++ pinwall-server/app/controller/wx/users.js
@@ -5,86 +5,83 @@ var rp = require('request-promise');
 
 class UsersController extends BaseController {
 
-    async createWxUser() {
-        const ctx = this.ctx;
-        const email = ctx.request.body.email;
-        const fullname = ctx.request.body.fullname;
-        const password = ctx.request.body.password;
+  async createWxUser() {
+    const ctx = this.ctx;
+    const body = ctx.request.body;
+    const email = body.email;
+    const fullname = body.fullname;
+    const password = body.password;
 
-        if (ctx.user) {
-            let user = {
-                email: email,
-                fullname: fullname,
-                password: password,
-                openId: ctx.user.openid,
-                nickname: ctx.user.nickname,
-                gender: ctx.user.sex,
-                city: ctx.user.city,
-                province: ctx.user.province,
-                country: ctx.user.country,
-                avatarUrl: ctx.user.headimageurl,
-            };
-            try {
-                const result = await ctx.service.users.createUser(user, 1);
-                if (result) {
-                    super.success('操作成功！请到邮箱激活');
-                } else {
-                    super.failure('操作失败！请重新操作');
-                }
-            } catch (e) {
-                super.failure(e.message);
-            }
-
-        } else {
-            super.failure('微信扫描信息有误，请重新扫描!');
-        }
+    let user = {
+      email: email,
+      fullname: fullname,
+      password: password,
+      openId: body.openid,
+      nickname: body.nickname,
+      gender: body.sex,
+      city: body.city,
+      province: body.province,
+      country: body.country,
+      avatarUrl: body.headimageurl,
+    };
+    try {
+      const result = await ctx.service.users.createUser(user, 1);
+      if (result) {
+        super.success('操作成功！请到邮箱激活');
+      } else {
+        super.failure('操作失败！请重新操作');
+      }
+    } catch (e) {
+      super.failure(e.message);
     }
 
-    async bindWeixinInfoByEmail() {
-        const ctx = this.ctx;
-        const body = ctx.request.body;
-        const email = body.email;
-        let user = {
-            openId: body.openid,
-            nickname: body.nickname,
-            avatarUrl: body.headimageurl,
-            gender: body.sex,
-            province: body.province,
-            city: body.city,
-            country: body.country
-        };
+  }
 
-        const result = await ctx.service.users.bindWeixinInfoByEmail(email, user);
+  async bindWeixinInfoByEmail() {
+    const ctx = this.ctx;
+    const body = ctx.request.body;
+    const email = body.email;
+    let user = {
+      openId: body.openid,
+      nickname: body.nickname,
+      avatarUrl: body.headimageurl,
+      gender: body.sex,
+      province: body.province,
+      city: body.city,
+      country: body.country
+    };
 
-        if (result) {
-            super.success('绑定成功，请进入邮箱激活!');
-        } else {
-            super.failure('绑定失败!');
-        }
+    const result = await ctx.service.users.bindWeixinInfoByEmail(email, user);
+
+    if (result) {
+      super.success('绑定成功，请进入邮箱激活!');
+    } else {
+      super.failure('绑定失败!');
     }
+  }
 
-    async getWxCode() {
-        const ctx = this.ctx;
-        const jscode = ctx.query.jscode;
+  async getWxCode() {
+    const ctx = this.ctx;
+    const jscode = ctx.query.jscode;
 
-        const requestUrl = `https://api.weixin.qq.com/sns/jscode2session?appid=wxa4cd6f777c8b75d0&secret=aeb6d1ab0c59d4145bd00e146551f468&js_code=${jscode}&grant_type=authorization_code`;
+    const requestUrl = `https://api.weixin.qq.com/sns/jscode2session?appid=wxa4cd6f777c8b75d0&secret=aeb6d1ab0c59d4145bd00e146551f468&js_code=${jscode}&grant_type=authorization_code`;
 
-        let data;
-        await rp(requestUrl).promise().bind(this).then(function (repos) {
-            data = repos;
-        });
+    let data;
+    await rp(requestUrl).promise().bind(this).then(function(repos) {
+      data = repos;
+    });
 
-        let openid = JSON.parse(data).openid;
+    let openid = JSON.parse(data).openid;
 
-        let result = {
-            openid:openid,
-        }
-        if(openid){
-            const user = await ctx.service.users.findByOpenId(openid);
-            result.user = user;
-        }
-        ctx.body = result;
+    let result = {
+      openid: openid,
     }
+    if (openid) {
+      const user = await ctx.service.users.findByOpenId(openid);
+      result.user = user;
+    }
+    ctx.body = result;
+  }
 
 }
 
